03d52f67c6926bbac4b235bde2dcc9d8c046e5ab
323liwanyue <smileliwanyue@foxmail.com>
2018-11-27 10:59:40 +0800

    miniprogram update
    update

--- app.js
+++ app.js
@@ -5,14 +5,35 @@ App({
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
+    wx.setStorageSync('logs', logs);
+    const vm = this
+    // let totalTopHeightSet = {
+    //   'iPhone': 64,
+    //   'iPhone X': 88,
+    //   'android': 68
+    // }
+    wx.getSystemInfo({
+      success: function (res) {
+        let totalTopHeight = 68
+        if (res.model.indexOf('iPhone X') !== -1) {
+          totalTopHeight = 88
+        } else if (res.model.indexOf('iPhone') !== -1) {
+          totalTopHeight = 64
+        }
+        vm.globalData.statusBarHeight = res.statusBarHeight
+        vm.globalData.titleBarHeight = totalTopHeight - res.statusBarHeight
+      },
+      failure() {
+        vm.globalData.statusBarHeight = 0
+        vm.globalData.titleBarHeight = 0
+      }
+    }),
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
-    })
+    });
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -21,7 +42,7 @@ App({
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+              this.globalData.userInfo = res.userInfo;
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
@@ -37,4 +58,4 @@ App({
   globalData: {
     userInfo: null
   }
-})
+});
